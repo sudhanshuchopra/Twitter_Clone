a5efafa0c7ab758fb4cce6a4a0d9c23dfafe570f
sudhanshu <sudhanshuccp@gmail.com>
2017-10-31 01:08:56 +0530

    tweets app added

---
+++ src/tweets/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Tweet(models.Model):
+	content=models.CharField(max_length=140)
+	timestamp=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return str(self.content)
